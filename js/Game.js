5ed8dcbed888a7a12c14ba01d05a700ed4fbf75e
Kim Drezdon <kdrezdon@gmail.com>
2019-03-16 12:25:08 -0500

    added basic structure to game and phrase class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor () {
+        this.missed = 
+        this.phrases = 
+        this.activePhrase = 
+    }
+
+    startGame () {
+
+    }
+
+    getRandomPhrase () {
+
+    }
+
+    handleInteraction () {
+
+    }
+
+    removeLife () {
+
+    }
+
+    checkForWin () {
+
+    }
+
+    gameOver () {
+        
+    }
+}

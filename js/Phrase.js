45d45bbfdc226425ae89ced5e461b46c3d906f0f
Kim Drezdon <kdrezdon@gmail.com>
2019-03-17 15:21:55 -0500

    created addPhraseToDisplay method and added comments

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,19 +3,36 @@
  * Phrase.js */
 
  class Phrase {
-     constructor (phrase) {
-        this.phrase: phrase.toLowerCase();
-     }
+    constructor (phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
 
-     addPhraseToDisplay () {
+    /*  adds letter placeholders to the display when the game starts, each 
+    letter represented by an empty box, one li element for each letter */
+    addPhraseToDisplay () {
+        const phraseUl = document.querySelector('#phrase ul');
+        for (let char of this.phrase) {
+            const li = document.createElement('li');
+            if (char !== ' ') {
+                li.className = `hide letter ${char}`;
+            } else {
+                li.className = 'space';
+            }
+            li.textContent = char;
+            phraseUl.appendChild(li);
+        }
+    }
 
-     }
+    //checks to see if the letter selected by the player matches a letter in the phrase
+    checkLetter () {
 
-     checkLetter () {
+    }
 
-     }
-
-     showMatchedLetter () {
-         
-     }
- }
+    /*  reveals the letters on the board that matches the player's selection. 
+    To reveal the matching letters select all of the letter DOM elements 
+    that have a CSS class name that matches the selected letter and replace 
+    each selected elements hide CSS class with the show CSS class */
+    showMatchedLetter () {
+        
+    }
+}
